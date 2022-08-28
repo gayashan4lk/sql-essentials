dc2a0169554bc4504e88fa2cf835dd73f2e54f17
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-08-28 22:58:17 +0530

    Join

---
+++ 3-relations.sql
@@ -0,0 +1,40 @@
+-- ## JOIN ##
+-- test.db
+
+CREATE TABLE left ( id INTEGER, description TEXT );
+CREATE TABLE right ( id INTEGER, description TEXT );
+
+INSERT INTO left VALUES ( 1, 'left 01' );
+INSERT INTO left VALUES ( 2, 'left 02' );
+INSERT INTO left VALUES ( 3, 'left 03' );
+INSERT INTO left VALUES ( 4, 'left 04' );
+INSERT INTO left VALUES ( 5, 'left 05' );
+INSERT INTO left VALUES ( 6, 'left 06' );
+INSERT INTO left VALUES ( 7, 'left 07' );
+INSERT INTO left VALUES ( 8, 'left 08' );
+INSERT INTO left VALUES ( 9, 'left 09' );
+
+INSERT INTO right VALUES ( 6, 'right 06' );
+INSERT INTO right VALUES ( 7, 'right 07' );
+INSERT INTO right VALUES ( 8, 'right 08' );
+INSERT INTO right VALUES ( 9, 'right 09' );
+INSERT INTO right VALUES ( 10, 'right 10' );
+INSERT INTO right VALUES ( 11, 'right 11' );
+INSERT INTO right VALUES ( 11, 'right 12' );
+INSERT INTO right VALUES ( 11, 'right 13' );
+INSERT INTO right VALUES ( 11, 'right 14' );
+
+SELECT * FROM left;
+SELECT * FROM right;
+
+-- INNER JOIN
+
+SELECT l.id AS leftID, l.description AS left, r.id AS rightID, r.description AS right 
+    FROM left AS l JOIN right AS r ON l.id = r.id;
+
+-- OUTER JOIN
+
+SELECT l.id AS leftID, l.description AS left, r.id AS rightID, r.description AS right 
+    FROM left AS l LEFT JOIN right AS r ON l.id = r.id;
+
+    
